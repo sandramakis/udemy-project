24f78e2d065ac1acf3a0e52d30716aa5006a43a2
Sandra Nwakeze <sandramakis.dev@gmail.com>
2023-03-10 18:25:26 +0100

    Challenge on Basic Operators

--- assignment.js
+++ assignment.js
@@ -20,7 +20,7 @@ console.log(typeof language); //undefined
 language = "Igbo";
 
 // Basic Operators
-// Question: If the ourPopulation of my country was split into two, what would be the ourPopulation on one side?
+// Question: If the Population of my country was split into two, what would be the ourPopulation on one side?
 
 let halfPopulation = ourPopulation / 2;
 console.log(halfPopulation); //107.2
@@ -40,3 +40,45 @@ console.log(ourPopulation <= averagePopulation);
 // PROBLEM: Based on thye variables I created, create a new variable "description" which containsa string in this format "Portugal is in Europe, and its 11 million people speak Portguese"
 
 let description = "Nigeria is in Africa, and its 50 million people speak Igbo ";
+
+// CODING CHALLENGE ON OPERATORS
+/* Problem: 
+Mark and John are trying to compare their BMI (Body Mass Index), which is calculated using the formular: BMI = mass / height ** 2 = mass / (height * height). (mass in kg, height in meter)
+
+1. Store Mark's and John's mass and height in variables
+2. Calculate both their BMIs using the formular (you can implement both versions)
+3. Create a boolean variable "markHigherBMI" containing information about whether Mark has a higher BMI than John
+
+
+TEST DATA 1: Mark's weight = 78kg, height = 1.69m tall
+             John's weight = 92kg, height = 1.95m tall
+TEST DATA 2: Mark's weight = 95kg, height = 1.88m tall
+             John's weight = 85kg, height = 1.76m tall
+*/
+
+// SOLUTION
+// First Test Data
+const massMark = 78;
+const heightMark = 1.69;
+const heightJohn = 1.95;
+const massJohn = 92;
+
+const markBMI = massMark / heightMark ** 2;
+const BMIJohn = massJohn / heightJohn ** 2;
+console.log(markBMI, BMIJohn); //Output: 27.309968138370508 24.194608809993426
+
+const markHigherBMI = markBMI > BMIJohn;
+console.log(markHigherBMI); //Output: True
+
+// Second Test Data
+const secondMassMark = 95;
+const secondHeightMark = 1.88;
+const secondMassJohn = 85;
+const secondHeightJohn = 1.76;
+
+const secondMarkBMI = secondMassMark / secondHeightMark ** 2;
+const secondJohnBMI = secondMassJohn / secondHeightJohn ** 2;
+console.log(secondMarkBMI, secondJohnBMI); //Output: 26.87867813490267 27.44059917355372
+
+const johnHigherBMI = secondMarkBMI > secondJohnBMI;
+console.log(johnHigherBMI); //Output: false
