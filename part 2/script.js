afd28acb566b70ffbceff12454d232fd35ed936c
Sandra Nwakeze <sandramakis.dev@gmail.com>
2023-05-03 23:44:18 +0100

    ADD: Arrow Function & Function inside another function Assignment

--- part 2/script.js
+++ part 2/script.js
@@ -47,7 +47,61 @@ const percentageOfWorld2 = function (population) {
 };
 
 console.log(
-  percentageOfWorld2(200),
-  percentageOfWorld2(530),
-  percentageOfWorld2(910)
+  percentageOfWorld2(200), // 2.5316455696202533
+  percentageOfWorld2(530), // 6.70886075949367
+  percentageOfWorld2(910) // 11.518987341772153
 );
+
+// FUNCTION IN ANOTHER FUNCTION
+
+const describePopulation = (country, population) => {
+  const percCountry = percentageOfWorld2(population); //The population parameter here takes the argument passed in the calling of the function describePopulation. percentageOfWorld(population) == describePopulation(population)
+  return `${country} has ${population} million people which is about ${percCountry} of the world.`;
+};
+
+console.log(describePopulation("China", 1441));
+console.log(describePopulation("Dahomey", 240));
+console.log(describePopulation("Finland", 40));
+
+// ARROW FUNCTION
+// Rewrite the previous code with an arrow function
+
+const percentageOfWorld3 = (population) => (population / 7900) * 100;
+const percGhana = percentageOfWorld3(238);
+const percSweden = percentageOfWorld3(38);
+const percFinland = percentageOfWorld3(190);
+
+console.log(percGhana, percSweden, percFinland);
+
+// Calculate the remaining years of retirement for John
+const calcAge = (birthYear) => 2023 - birthYear;
+
+console.log(calcAge(1997));
+
+const remainingYearsOfRetirement = (birthYear, firstName) => {
+  const age = 2023 - birthYear;
+  const retirement = 65 - age;
+  return `${firstName} will be retiring in ${retirement} years`;
+};
+
+console.log(remainingYearsOfRetirement(1997, "Jonas"));
+console.log(remainingYearsOfRetirement(2001, "Makis"));
+
+// MANUAL WAY OF WRITING CODE
+const birthYear = 2001;
+const firstName = "Makis";
+const age = 2023 - birthYear;
+const retirement = 65 - age;
+console.log(
+  `${firstName} is currently ${age}, so will be retiring in the next ${retirement} years`
+);
+
+// ADVANCED WAY OF WRITING (DRY- Don't Repeat Yourself)
+
+const yearsOfRetirement = (birthYear, firstName) => {
+  const age = 2023 - birthYear;
+  const retirement = 65 - age;
+  return `${firstName} is currently ${age}, so will be retiring in the next ${retirement} years`;
+};
+
+console.log(yearsOfRetirement(2001, "Makis"));
