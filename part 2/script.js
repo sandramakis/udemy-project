bc31cc815aee5d09edd2be008e6fd68b628d9025
Sandra Nwakeze <sandramakis.dev@gmail.com>
2023-05-15 15:11:59 +0100

    ADD: Code Challenge #3

--- part 2/script.js
+++ part 2/script.js
@@ -245,5 +245,39 @@ console.log(jonas.calcAge());
 console.log(jonas.age);
 console.log(jonas.getSummary()); //Calling the getSummary function and logging it to the console. Output: Jonas is a 38-year old teacher and he has a driver's license
 
-// Challenge #3
+// Code Challenge #3
 // PROBLEM: Calculate John Smith and Mark Miller's BMI and retun who's BMI is higher
+
+const mark = {
+  fullName: "Mark Miller",
+  mass: 78,
+  height: 1.69,
+  calcBMI: function () {
+    this.bmi = this.mass / this.height ** 2;
+    return this.bmi;
+  },
+};
+
+const john = {
+  fullName: "John Smith",
+  mass: 92,
+  height: 1.95,
+  calcBMI: function () {
+    this.bmi = this.mass / this.height ** 2;
+    return this.bmi;
+  },
+};
+
+mark.calcBMI();
+john.calcBMI();
+console.log(john.bmi, mark.bmi);
+
+if (mark.bmi > john.bmi) {
+  console.log(
+    `${mark.fullName}'s BMI (${mark.bmi}) is higher than ${john.fullName}'s BMI (${john.bmi})`
+  );
+} else if (john.bmi > mark.bmi) {
+  console.log(
+    `${john.fullName}'s BMI (${john.bmi}) is higher than ${mark.fullName}'s BMI (${mark.bmi})`
+  );
+}
